486d31c4764054aa5c1b541dafdf3ec98ece7eb0
mohaa <mohaa.r.6789@gmail.com>
2021-12-17 18:48:24 +0530

    config file added for easy usage

---
+++ packages/jsdk-cli/src/config.js
@@ -0,0 +1,38 @@
+const MAIN_TITLE = '<< JSDK COMPILER >>'
+const DESC = 'A modern way to compile jsdk.'
+
+const HELP = [
+  '--help:shows help',
+  '--compile:helps to compile file into jsdk',
+  '--examples:shows sample commands',
+]
+
+const COMMANDS = {
+  compile: [
+    {
+      title: 'compile commands',
+      list: [
+        `jsdk --compile file=<FILE_NAME>`,
+        `jsdk --c file=<FILE_NAME>`,
+        `jsdk --compile folder=<FOLDER_NAME>`,
+        `jsdk --c folder=<FOLDER_NAME>`,
+      ],
+    },
+    {
+      title: 'compile commands with path',
+      list: [
+        `jsdk --compile file=<FILE_NAME> path=<FILE_PATH>`,
+        `jsdk --c file=<FILE_NAME> path=<FILE_PATH>`,
+        `jsdk --compile folder=<FOLDER_NAME> path=<FOLDER_PATH>`,
+        `jsdk --c folder=<FOLDER_NAME> path=<FOLDER_PATH>`,
+      ],
+    },
+  ],
+}
+
+module.exports = {
+  MAIN_TITLE,
+  DESC,
+  HELP,
+  COMMANDS,
+}
