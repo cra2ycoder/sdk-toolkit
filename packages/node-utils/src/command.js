2bfa92adc433b4f75955428738dc5d509f7a715e
mohaa <mohaa.r.6789@gmail.com>
2021-12-18 20:35:52 +0530

    additional set of scripts added

--- packages/node-utils/src/command.js
+++ packages/node-utils/src/command.js
@@ -1,4 +1,5 @@
 const shell = require('shelljs')
+const { execSync } = require('child_process')
 
 const createFolder = nameOrPath => shell.mkdir('-p', nameOrPath)
 
@@ -8,4 +9,70 @@ const removeFile = nameOrPath => shell.rm('-rf', nameOrPath)
 
 const copyFile = (srcFile, destFile) => shell.cp('-r', srcFile, destFile)
 
-module.exports = { createFolder, removeFolder, removeFile, copyFile }
+/**
+ * @see
+ * https://nodejs.org/api/child_process.html#child_process_child_process_execsync_command_options
+ */
+const run = (cmd = '', options = {}) => {
+  const { env, ...rest } = options
+  // console.log({ rest })
+
+  try {
+    execSync(cmd, {
+      stdio: 'inherit',
+      env: {
+        ...process.env,
+        NODE_ENV: env || 'development',
+        ...rest,
+      },
+    })
+  } catch (syncException) {
+    // printMessage('could not exec command' + syncException)
+  }
+}
+
+/**
+ * @note
+ * compile the file with node
+ */
+const runNode = (script = '', opts = undefined) => {
+  run(`node ${script}`, opts)
+}
+
+/**
+ * @note
+ * rollup commands will be done here
+ */
+const runRollup = (filePath = '', opts = undefined) => {
+  run(`rollup -c ${filePath}`, opts)
+}
+
+/**
+ * @note
+ * remove/delete the dist folder
+ */
+const cleanDir = (dir = undefined) => {
+  //   runNode('scripts/clean.js', {
+  //     DIR: dir,
+  //   })
+}
+
+/**
+ * @note
+ * watching the node server on each changes
+ */
+const watchNode = (script = '', opts = undefined) => {
+  run(`npx nodemon ${script}`, opts)
+}
+
+module.exports = {
+  createFolder,
+  removeFolder,
+  removeFile,
+  copyFile,
+  run,
+  runNode,
+  runRollup,
+  watchNode,
+  cleanDir,
+}
