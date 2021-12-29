db2a7a2f75e52b205caac298b9b0a359352346f7
mohaa <mohaa.r.6789@gmail.com>
2021-12-29 21:31:39 +0530

    basic compile script included

--- packages/jsdk-cli/rollup/compile.js
+++ packages/jsdk-cli/rollup/compile.js
@@ -1,8 +1,23 @@
-const { getRollupConfig } = require('../src/prompts')
+const { runRollup } = require('@xb/node-utils')
+const { getRollupConfig, getFileName } = require('../src/prompts')
+
+// console.log(require('@xb/node-utils'))
 
 const compileNow = options => {
   // console.log({ options })
-  const value = getRollupConfig(options)
+  let value = {
+    type: 'iife',
+    fileName: getFileName(options.inputs),
+  }
+
+  if (options.skipPrompt === false) {
+    value = getRollupConfig(options)
+  }
+
+  console.log(runRollup)
+
+  // runRollup('./rollup.config.js', {})
+
   console.log({ value })
 }
 
