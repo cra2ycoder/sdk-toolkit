7691fe6f81bca55589e874b5f1b9b5e0a32c139a
mohaa <mohaa.r.6789@gmail.com>
2022-01-14 23:27:07 +0530

    basic rollup configuration added

--- packages/jsdk-cli/rollup/rollup.config.js
+++ packages/jsdk-cli/rollup/rollup.config.js
@@ -1,7 +1,20 @@
+const path = require('path')
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+
+/**
+ * @description
+ * extracting the inputs from process.env
+ */
+const { fileLocation: input, type: format, bundleName } = process.env
+
+const outputFileLocation = `${input.substring(0, input.lastIndexOf('/'))}`
+const outputFileName = `${bundleName}.js`
+
 export default {
-  input: 'jsdk/main.ts',
+  input,
   output: {
-    file: 'bundle.js',
-    format: 'cjs',
+    file: `${outputFileLocation}/${outputFileName}`
+    format,
   },
+  plugins: [nodeResolve()],
 }
