dfd6f8b0b0bf3b0d0665bc22a92785afadba1b7c
mohaa <mohaa.r.6789@gmail.com>
2022-01-21 23:41:50 +0530

    lint bug fix & output name added for quick bundling

--- packages/jsdk-cli/rollup/rollup.config.ts
+++ packages/jsdk-cli/rollup/rollup.config.ts
@@ -1,13 +1,11 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
-console.log({ typescript })
-
 /**
  * @description
  * extracting the inputs from process.env
  */
-const { fileLocation: input, format, fileName } = process.env
+const { fileLocation: input = '', format, fileName } = process.env
 
 const outputFileLocation = `${input.substring(0, input.lastIndexOf('/'))}`
 const outputFileName = `${fileName}.js`
@@ -17,6 +15,7 @@ export default {
   output: {
     file: `${outputFileLocation}/${outputFileName}`,
     format,
+    name: fileName,
   },
   plugins: [nodeResolve(), typescript({ tsconfig: './tsconfig.json' })],
 }
