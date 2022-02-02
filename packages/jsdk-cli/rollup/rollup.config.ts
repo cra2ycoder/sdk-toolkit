9a936fd49fb4df26a2f1652244e582bc0d3ec2a8
mohaa <mohaa.r.6789@gmail.com>
2022-02-02 23:00:41 +0530

    multiple exporting options added

--- packages/jsdk-cli/rollup/rollup.config.ts
+++ packages/jsdk-cli/rollup/rollup.config.ts
@@ -1,5 +1,6 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import { terser } from 'rollup-plugin-terser'
 
 /**
  * @description
@@ -8,14 +9,39 @@ import typescript from '@rollup/plugin-typescript'
 const { fileLocation: input = '', format, fileName } = process.env
 
 const outputFileLocation = `${input.substring(0, input.lastIndexOf('/'))}`
-const outputFileName = `${fileName}.js`
+const outputFileName = `${fileName}`
 
 export default {
   input,
-  output: {
-    file: `${outputFileLocation}/${outputFileName}`,
-    format,
-    name: fileName,
-  },
+  // output: {
+  //   file: `${outputFileLocation}/${outputFileName}`,
+  //   format,
+  //   name: fileName,
+  // },
+  output: [
+    {
+      file: `${outputFileLocation}/${outputFileName}.iife.js`,
+      format: 'iife',
+      name: fileName,
+    },
+    { file: `${outputFileLocation}/${outputFileName}.cjs.js`, format: 'cjs' },
+    {
+      file: `${outputFileLocation}/${outputFileName}.min.js`,
+      format: 'cjs',
+      plugins: [
+        terser({
+          output: {
+            comments: (node, comment) => {
+              if (comment.type === 'comment2') {
+                return /@preserve|@license|@cc_on/i.test(comment.value)
+              }
+              return true
+            },
+          },
+        }),
+      ],
+    },
+    { file: `${outputFileLocation}/${outputFileName}.esm.js`, format: 'esm' },
+  ],
   plugins: [nodeResolve(), typescript({ tsconfig: './tsconfig.json' })],
 }
